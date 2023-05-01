3daf07d7b8c915b64988bedcccf4f015551f8a7e
Sara Restrepo Vélez <89092194+SaraRestrepoV@users.noreply.github.com>
2023-05-01 16:35:13 -0500

    New WebAPI Shopping project with country entity

---
+++ ShoppingWebAPI/ShoppingWebAPI/Entitiess/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.Entitiess
+{
+    public class Country
+    {
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")] // not null
+        public string Name { get; set; }
+    }
+}
---
+++ ShoppingWebAPI/ShoppingWebAPI/Entitiess/Entitycs.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebAPI.Entitiess
+{
+    public class Entitycs
+    {
+        [Key] //DataAnnotations - decoradores
+        public Guid Id { get; set; }
+        public DateTime? CreatedDate { get; set; } //? Puede ser nulo
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
